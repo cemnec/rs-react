abfe39b1f712871d37dcfd74885040a4a3e28c5c
IPetrushkina <irina.petrushkina@nixsolutions.com>
2026-05-10 08:42:52 +0300

    test: configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
+});
